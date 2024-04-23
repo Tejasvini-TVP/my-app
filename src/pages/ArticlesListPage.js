e79ae7c9d0c642db95dd08a25e1571c02934baaf
Tejasvin-TVP <Tejasvini.patel1311@gmail.com>
2024-04-23 18:23:24 +0530

    ArticlesListPage

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,7 +1,17 @@
-
+import articles from "./article-content";
+import { Link } from "react-router-dom";
 const ArticlesListPage = () => {
     return (
-      <h1>This is the  articles list page!</h1>
+      <>
+      <h1>articles</h1>
+      {articles.map(article => (
+        <Link className="article-list-item" to={`/articles/${article.name}`}>
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0,150)}...</p>
+        </Link>
+      ))}
+      </>
+
     )
   }
   
