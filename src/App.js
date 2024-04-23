c6d306c97893c8512a4870d34d19a04ac3ca138c
Tejasvin-TVP <Tejasvini.patel1311@gmail.com>
2024-04-23 17:41:47 +0530

    articles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import NavBar from './NavBar';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 
+
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>My Awesome Blog</h1>
+     <NavBar />
       <div id='page-body'>
        <Routes>
        <Route path='/' element= {<HomePage />} />
