982741dc9b236118baa1403d31f105540297599e
Tejasvin-TVP <Tejasvini.patel1311@gmail.com>
2024-04-29 17:19:59 +0530

    number conti.. update

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,8 +1,14 @@
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import NotFoundPage from "./NotFoundPage";
+import { useEffect, useState } from "react";
 
 const ArticlePage = () => {
+    const [articleInfo,setArticleInfo] = useState({ upvotes:0,comments: []})
+    // number conti.. update
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10),comments: [] })
+    })
     const { articleId} = useParams();
     const article = articles.find(article => article.name === articleId);
 
@@ -14,6 +20,7 @@ const ArticlePage = () => {
     return (
        <>
         <h1>{article.title}</h1>
+        <p>This article has {articleInfo.upvotes} upvote(s)</p>
         {article.content.map(paragraph => (
             <p key={paragraph}>{paragraph}</p>
         ))}
